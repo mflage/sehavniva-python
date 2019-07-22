48ce7b363040127bd8f9edb8e14f944eb9bf7958
Marius Flage <marius@flage.org>
2019-07-22 09:58:18 +0200

    added the download url and also changed some formatting

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 from setuptools import setup
 
 setup(
-    name='sehavniva',
-    version='0.1',
-    packages=['sehavniva-python'],
-    url='https://github.com/mflage/sehavniva-python',
+    name = 'sehavniva-python',
+    version = '0.1',
+    packages = ['sehavniva-python'],
+    url = 'https://github.com/mflage/sehavniva-python',
+    download_url = "https://github.com/mflage/sehavniva-python/archive/v_01.tar.gz"
     keywords = ['kartverket', 'sehavniva', 'iot', 'tide', 'sensor', 'smarthome', 'automation'],
-    license='MIT',
-    author='Marius Flage',
-    author_email='marius@flage.org',
-    description='Unofficial Python library for the Norwegian Mapping Authority\'s SeHavNiva API',
-    install_requires=['requests', 'xmltodict'],
-    classifiers=[
+    license = 'MIT',
+    author = 'Marius Flage',
+    author_email = 'marius@flage.org',
+    description = 'Unofficial Python library for the Norwegian Mapping Authority\'s SeHavNiva API',
+    install_requires = ['requests', 'xmltodict'],
+    classifiers = [
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries :: Python Modules',
